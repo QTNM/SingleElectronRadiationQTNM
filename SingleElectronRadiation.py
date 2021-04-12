d1e532d3db1e905845b89d1505b66b11f7ccfdf5
DSwinnockPhysics <72457781+DSwinnockPhysics@users.noreply.github.com>
2021-04-12 15:19:48 +0100

    Added Power Loss and Relativistic E Field
    Added power loss for basic motion calculations.
    Added near electric field (non relativistic)
    Added far and near relativistic electric fields.

--- SingleElectronRadiation.py
+++ SingleElectronRadiation.py
@@ -17,7 +17,7 @@ EVelocity = numpy.array([0,0,0]) # Electron velocity at retarded time
 EAcceleration = numpy.array([0,0,0]) # Electron acceleration at retarded time
 EPosition = numpy.array([0,0,0])
 EPositionRet = numpy.array([0,0,0]) # Electron retarted position (Electron position evaluated at retarded time)
-EEnergy = 18600*ECharge # Electron Energy in Joules, 18.6keV
+EEnergy = 18600*ECharge # Electron Energy in Joules, 18.6keV # Initial Energy
 
 
 # Function definitions
@@ -26,7 +26,6 @@ def CalcNonRelEField(Position,Time,EPosition,EAcceleration):
     # Calculates the non-relativistic electric field at a given position and time
     # for an electron located at EPosition, with acceleration given.
     # This formula assumes non relativistic speeds so the emission point = the position of the electron.
-    
     # First calculate vector/unit vector between position and emission point
     r = numpy.linalg.norm(Position-EPosition) # Distance from electron (radiation point) to detector
     VecEmissionToObsUnit = (Position-EPosition)/r # Unit vector in direction from emission point to detector position
@@ -35,6 +34,13 @@ def CalcNonRelEField(Position,Time,EPosition,EAcceleration):
     EField = ECharge/(4*pi*VacPerm*c)*(numpy.cross(n,numpy.cross(n,EAcceleration)/c)/r)
     return EField
 
+def CalcNonRelNearEField(Position,Time,EPosition,EVelocity,EAcceleration):
+    # Calculates the near field non-relativistically
+    r = numpy.linalg.norm(Position-EPosition) # Distance from electron (radiation point) to detector
+    VecEmissionToObsUnit = (Position-EPosition)/r # Unit vector in direction from emission point to detector position
+    n = VecEmissionToObsUnit
+    EFieldNear = ECharge/(4*pi*VacPerm) * (n/r**2) # I need a vector
+    return EFieldNear
 
 def CalcElectronCircularMotion(Time,AngFreq,Amplitude,ZPos):
     # Circular Motion around Z axis at Zpos
@@ -61,7 +67,6 @@ def CreateDetectorArray(Resolution,x0,x1,y0,y1,z):
     #print(DetectorPositionArray) # [0][0] gives top left corner [y][x]
     return DetectorPositionArray
 
-
 def CalcNonRelEFieldArray(DetectorPositionArray,Time,EPosition,EAcceleration,Resolution):
     EFieldArray = numpy.zeros((Resolution,Resolution,3))
     for i in range(Resolution):
@@ -70,11 +75,17 @@ def CalcNonRelEFieldArray(DetectorPositionArray,Time,EPosition,EAcceleration,Res
             EFieldArray[i,j] = EFieldVector
     return EFieldArray
 
+def CalcNonRelNearEFieldArray(DetectorPositionArray,Time,EPosition,EVelocity,EAcceleration,Resolution):
+    NearEFieldArray = numpy.zeros((Resolution,Resolution,3))
+    for i in range(Resolution):
+        for j in range(Resolution):
+            NearEFieldVector = CalcNonRelNearEField(DetectorPositionArray[i][j],Time,EPosition,EVelocity,EAcceleration)
+            NearEFieldArray[i,j] = NearEFieldVector
+    return NearEFieldArray    
 
 def CalcPoyntingVectorMagnitude(EFieldMagnitudes):
     PoyntingMagnitudes = VacPerm*c*EFieldMagnitudes**2
     return PoyntingMagnitudes
-
 
 def FindColourBounds(InputArray,TimeResolution):
     ColourMax = numpy.max(InputArray[0])
@@ -87,8 +98,45 @@ def FindColourBounds(InputArray,TimeResolution):
         if ColourMin > NewMin:
             ColourMin = NewMin
     return(ColourMin,ColourMax)
-    
+  
+def CalcLarmorPower(EAcceleration):
+    return 2/3 * ECharge**2 * (numpy.linalg.norm(EAcceleration))**2  / 4 / pi / VacPerm / c**3
 
+def CalcCyclotronFreqWithPowerLoss(PowerLoss,BField):
+    Premultiplier = ECharge*BField/(EMass+EEnergy/c**2)
+    return Premultiplier*(1+PowerLoss/(EMass*c**2+EEnergy))
+
+def CalcCyclotronAmpWithPowerLoss(PowerLoss,BField):
+    v = (2*(EEnergy-PowerLoss)/EMass)**0.5 # velocity from K.E incorporating power loss
+    return EMass * v / ECharge / BField
+
+
+def CalcRelFarEField(Position,Time,EPosition,EVelocity,EAcceleration):
+    # Calculates the relativistic electric field at a given position (far field)
+    # for an electron located at EPosition, with acceleration and velocity given.
+    Premultiplier = ECharge/(4*pi*VacPerm*c)
+    r = numpy.linalg.norm(Position-EPosition) # Distance from electron (radiation point) to detector
+    n = (Position-EPosition)/r # Unit vector in direction from emission point to detector position
+    Premultiplier = Premultiplier * 1 / (1-numpy.dot(n,EVelocity)/c)**3
+    FarFieldPart = numpy.cross( n , numpy.cross( (n-EVelocity/c) , EAcceleration/c ) ) / numpy.linalg.norm(Position-EPosition)
+    RelFarEField = Premultiplier*FarFieldPart
+    # And the time delay before it arrives at the detector will also be returned.
+    TimeDelay = r/c # seconds
+    return RelFarEField,TimeDelay
+
+def CalcRelNearEField(Position,Time,EPosition,EVelocity,EAcceleration):
+    # Calculates the relativistic electric field at a given position (near field)
+    # for an electron located at EPosition, with acceleration and velocity given.
+    Premultiplier = ECharge/(4*pi*VacPerm) 
+    r = numpy.linalg.norm(Position-EPosition) # Distance from electron (radiation point) to detector
+    n = (Position-EPosition)/r # Unit vector in direction from emission point to detector position
+    Premultiplier = Premultiplier * 1/(1-numpy.dot(n,EVelocity)/c)**3
+    
+    NearFieldPart = (1-(numpy.linalg.norm(EVelocity)/c)**2)*(n-EVelocity/c) / (numpy.linalg.norm(Position-EPosition))**2
+    RelNearEField = Premultiplier * NearFieldPart
+    TimeDelay = r/c # seconds
+    return RelNearEField,TimeDelay
+    
 def RunSingleElectronRadiation():
     # Define the detector's position
     Resolution = 10
@@ -99,25 +147,20 @@ def RunSingleElectronRadiation():
     
     # Calculate circular motion
     Time=0
-    AngFreq = 1.7563*10**11 # s^-1
-    Amplitude = 0.000448 # m
-    # EPosition, EVelocity, EAcceleration = CalcElectronCircularMotion(Time,AngFreq,Amplitude,ZPos) # This is done later in the loop
+    BField = 1 # Tesla
+    AngFreq = CalcCyclotronFreqWithPowerLoss(0,BField) # 1.7563*10**11 # s^-1
+    print("AngFreq=",AngFreq)
     
-#    # Calculate E Field for circular motion
-#    EFieldArray = CalcNonRelEFieldArray(DetectorPositionArray,Time,EPosition,EAcceleration,Resolution) # EFieldArray[xindex, yindex]
-#    EFieldArrayXComponents = EFieldArray[0:,0:,0]
-#    EFieldArrayYComponents = EFieldArray[0:,0:,1]
-#    EFieldArrayZComponents = EFieldArray[0:,0:,2]
-#    EFieldMagnitudes = numpy.linalg.norm(EFieldArray,axis=(2))
-#    PoyntingMagnitudes = CalcPoyntingVectorMagnitude(EFieldMagnitudes)
+    # Amplitude = 0.000448 # m
+    Amplitude = CalcCyclotronAmpWithPowerLoss(0,BField)
+    
 
     # Calculate time steps
-    TimeResolution = 15
+    TimeResolution = 30
     MaxTime = 2*pi/AngFreq # 1 Loop around the circle
     TimeStep = MaxTime/TimeResolution
     TimesUsed = []
-#    ColourMaxEMag = -999999
-#    ColourMinEMag = +999999
+
     EFieldXs = numpy.zeros(TimeResolution, dtype=object)
     EFieldYs = numpy.zeros(TimeResolution, dtype=object)
     EFieldZs = numpy.zeros(TimeResolution, dtype=object)
@@ -127,13 +170,25 @@ def RunSingleElectronRadiation():
     EPositionYs = numpy.zeros(TimeResolution)
     EPositionZs = numpy.zeros(TimeResolution)
     
+    AngFreqArray = numpy.zeros(TimeResolution)
+    AmplitudeArray = numpy.zeros(TimeResolution)
+    LarmorPowerArray = numpy.zeros(TimeResolution)
+    PowerLoss = 0 # Initialise total power loss, this updates as the larmor power changes in the loop
+    
     for i in range(TimeResolution):
+        print(i)
         # Calculate electron motion for each time step
         ZPos = 0
+        
+        # Update cyclotron frequency and amplitude based on total power loss
+        AngFreq = CalcCyclotronFreqWithPowerLoss(PowerLoss, BField)
+        Amplitude = CalcCyclotronAmpWithPowerLoss(PowerLoss, BField)
+        
         EPosition, EVelocity, EAcceleration = CalcElectronCircularMotionPerp(Time,AngFreq,Amplitude,ZPos)
         
         # Calculate EField and Poynting for each time step
-        EFieldArray = CalcNonRelEFieldArray(DetectorPositionArray,Time,EPosition,EAcceleration,Resolution)
+        #EFieldArray = CalcNonRelEFieldArray(DetectorPositionArray,Time,EPosition,EAcceleration,Resolution)
+        EFieldArray = CalcNonRelEFieldArray(DetectorPositionArray,Time,EPosition,EAcceleration,Resolution) + CalcNonRelNearEFieldArray(DetectorPositionArray,Time,EPosition,EVelocity,EAcceleration,Resolution)
         EFieldMagnitudes = numpy.linalg.norm(EFieldArray,axis=(2))    
         EFieldXs[i] = EFieldArray[0:,0:,0]
         EFieldYs[i] = EFieldArray[0:,0:,1]
@@ -147,6 +202,15 @@ def RunSingleElectronRadiation():
         
         TimesUsed.append(Time)
         Time = Time + TimeStep
+        
+
+        LarmorPower = CalcLarmorPower(EAcceleration)
+        PowerLoss += LarmorPower * TimeStep
+        
+        LarmorPowerArray[i] = LarmorPower
+        AngFreqArray[i] = AngFreq
+        AmplitudeArray[i] = Amplitude
+        
     # end loop
     
     # Calculate Power from Poynting vector
@@ -164,61 +228,60 @@ def RunSingleElectronRadiation():
     xs = DetectorPositionArray[0:,0:,0]
     ys = DetectorPositionArray[0:,0:,1]
 
-    #print(TimesUsed)
-     
-    for i in range(TimeResolution):
-        # Plot and output images
-        figsize = [7, 8]     # figure size, inches
-        fig1, ax = pyplot.subplots(nrows=2, ncols=1, figsize=figsize)
-        fig1graph0 = ax[0].imshow(EFieldMagnitudesArray[i]*10**6, extent=(x0,x1,y0,y1))
-        fig1graph1 = ax[1].imshow(PowerArray[i], extent=(x0,x1,y0,y1))
-        pyplot.xlabel("x (m)")
-        pyplot.ylabel("y (m)")
-        colourbar = pyplot.colorbar(fig1graph0, ax=ax[0], orientation="vertical")
-        colourbar.set_label(r"Magnitude ($\mu$V/m)")
-        colourbar = pyplot.colorbar(fig1graph1, ax=ax[1], orientation="vertical")
-        colourbar.set_label(r"Power Density (W/m$^2$)")
-        fig1graph0.set_clim(ColourMinEMag*10**6,ColourMaxEMag*10**6)
-        fig1graph1.set_clim(ColourMinPowerArray,ColourMaxPowerArray)
-        ax[0].set_title("E Field Magnitude, t=%ss" % '%.2g' % TimesUsed[i])
-        ax[1].set_title("Power")
-        pyplot.tight_layout()
-        fig1.savefig("Images\Poynting\Poynting%s" % i)
-        
-        fig2,ax2 = pyplot.subplots(nrows=1, ncols=1, figsize=[8,5])
-        fig2graph0 = ax2.quiver(xs,ys,EFieldXs[i],EFieldYs[i])
-        ax2.set_title("E Field XY Components, t=%ss" % '%.2g' % TimesUsed[i])
-        pyplot.xlabel("x (m)")
-        pyplot.ylabel("y (m)")
-        pyplot.tight_layout()
-        fig2.savefig("Images\Polarity\EFieldPolarity%s" % i)
-
-        pyplot.close(fig1) 
-        pyplot.close(fig2)
-        
-        fig4,ax4 = pyplot.subplots(nrows=3, ncols=1, figsize=[8,12])
-        fig4graph0 = ax4[0].imshow(EFieldXs[i]*10**6, extent=(x0,x1,y0,y1))
-        fig4graph1 = ax4[1].imshow(EFieldYs[i]*10**6, extent=(x0,x1,y0,y1))
-        fig4graph2 = ax4[2].imshow(EFieldZs[i]*10**6, extent=(x0,x1,y0,y1))
-        colourbar = pyplot.colorbar(fig4graph0, ax=ax4[0], orientation="vertical")
-        colourbar.set_label(r"Magnitude ($\mu$V/m)")
-        colourbar = pyplot.colorbar(fig4graph1, ax=ax4[1], orientation="vertical")
-        colourbar.set_label(r"Magnitude ($\mu$V/m)")
-        colourbar = pyplot.colorbar(fig4graph2, ax=ax4[2], orientation="vertical")
-        colourbar.set_label(r"Magnitude ($\mu$V/m)")
-        fig4graph0.set_clim(ColourMinEFXs*10**6,ColourMaxEFXs*10**6)
-        fig4graph1.set_clim(ColourMinEFYs*10**6,ColourMaxEFYs*10**6)
-        fig4graph2.set_clim(ColourMinEFZs*10**6,ColourMaxEFZs*10**6)
-        ax4[0].set_title("E Field x Component, t=%ss" % '%.2g' %TimesUsed[i])
-        ax4[1].set_title("E Field y Component")
-        ax4[2].set_title("E Field z Component")
-        pyplot.xlabel("x (m)")
-        pyplot.ylabel("y (m)")
-        pyplot.tight_layout()
-        fig4.savefig("Images\EFieldComponents\EFieldComponents%s" % i)
-        pyplot.close(fig4)
-        
-    # end loop
+    if TimeResolution < 50:
+        for i in range(TimeResolution):
+            # Plot and output images
+            figsize = [7, 8]     # figure size, inches
+            fig1, ax = pyplot.subplots(nrows=2, ncols=1, figsize=figsize)
+            fig1graph0 = ax[0].imshow(EFieldMagnitudesArray[i]*10**6, extent=(x0,x1,y0,y1))
+            fig1graph1 = ax[1].imshow(PowerArray[i], extent=(x0,x1,y0,y1))
+            pyplot.xlabel("x (m)")
+            pyplot.ylabel("y (m)")
+            colourbar = pyplot.colorbar(fig1graph0, ax=ax[0], orientation="vertical")
+            colourbar.set_label(r"Magnitude ($\mu$V/m)")
+            colourbar = pyplot.colorbar(fig1graph1, ax=ax[1], orientation="vertical")
+            colourbar.set_label(r"Power Density (W/m$^2$)")
+            fig1graph0.set_clim(ColourMinEMag*10**6,ColourMaxEMag*10**6)
+            fig1graph1.set_clim(ColourMinPowerArray,ColourMaxPowerArray)
+            ax[0].set_title("E Field Magnitude, t=%ss" % '%.2g' % TimesUsed[i])
+            ax[1].set_title("Power")
+            pyplot.tight_layout()
+            fig1.savefig("Images\Poynting\Poynting%s" % i)
+            
+            fig2,ax2 = pyplot.subplots(nrows=1, ncols=1, figsize=[8,5])
+            fig2graph0 = ax2.quiver(xs,ys,EFieldXs[i],EFieldYs[i])
+            ax2.set_title("E Field XY Components, t=%ss" % '%.2g' % TimesUsed[i])
+            pyplot.xlabel("x (m)")
+            pyplot.ylabel("y (m)")
+            pyplot.tight_layout()
+            fig2.savefig("Images\Polarity\EFieldPolarity%s" % i)
+    
+            pyplot.close(fig1) 
+            pyplot.close(fig2)
+            
+            fig4,ax4 = pyplot.subplots(nrows=3, ncols=1, figsize=[8,12])
+            fig4graph0 = ax4[0].imshow(EFieldXs[i]*10**6, extent=(x0,x1,y0,y1))
+            fig4graph1 = ax4[1].imshow(EFieldYs[i]*10**6, extent=(x0,x1,y0,y1))
+            fig4graph2 = ax4[2].imshow(EFieldZs[i]*10**6, extent=(x0,x1,y0,y1))
+            colourbar = pyplot.colorbar(fig4graph0, ax=ax4[0], orientation="vertical")
+            colourbar.set_label(r"Magnitude ($\mu$V/m)")
+            colourbar = pyplot.colorbar(fig4graph1, ax=ax4[1], orientation="vertical")
+            colourbar.set_label(r"Magnitude ($\mu$V/m)")
+            colourbar = pyplot.colorbar(fig4graph2, ax=ax4[2], orientation="vertical")
+            colourbar.set_label(r"Magnitude ($\mu$V/m)")
+            fig4graph0.set_clim(ColourMinEFXs*10**6,ColourMaxEFXs*10**6)
+            fig4graph1.set_clim(ColourMinEFYs*10**6,ColourMaxEFYs*10**6)
+            fig4graph2.set_clim(ColourMinEFZs*10**6,ColourMaxEFZs*10**6)
+            ax4[0].set_title("E Field x Component, t=%ss" % '%.2g' %TimesUsed[i])
+            ax4[1].set_title("E Field y Component")
+            ax4[2].set_title("E Field z Component")
+            pyplot.xlabel("x (m)")
+            pyplot.ylabel("y (m)")
+            pyplot.tight_layout()
+            fig4.savefig("Images\EFieldComponents\EFieldComponents%s" % i)
+            pyplot.close(fig4)
+            
+        # end loop
 
     fig3,ax3 = pyplot.subplots(nrows=3, ncols=1, figsize=[10,15])
     fig3graph0 = ax3[0].plot(TimesUsed,EPositionXs)
@@ -232,8 +295,32 @@ def RunSingleElectronRadiation():
     ax3[2].set_xlabel('t (s)')
     ax3[2].set_ylabel('z (m)')
     fig3.savefig("Images\Position\EPosition")
+    
+    figLarm,axLarm = pyplot.subplots(nrows=1, ncols=1, figsize=[11,10])
+    axLarm.plot(TimesUsed,LarmorPowerArray)
+    axLarm.set_title("Larmor Power")
+    axLarm.set_xlabel('t (s)')
+    axLarm.set_ylabel('Larmor Power (W)')
+    figLarm.savefig("Images\Misc\LarmorPower")
+    
+    figAngFreq,axAngFreq = pyplot.subplots(nrows=1, ncols=1, figsize=[11,10])
+    axAngFreq.plot(TimesUsed,AngFreqArray)
+    axAngFreq.set_title("Angular Frequency")
+    axAngFreq.set_xlabel('t (s)')
+    axAngFreq.set_ylabel('Angular Frequency (Hz)')
+    figAngFreq.savefig("Images\Misc\AngFreq")
+
+    figAmplitude,axAmplitude = pyplot.subplots(nrows=1, ncols=1, figsize=[11,10])
+    axAmplitude.plot(TimesUsed,AmplitudeArray)
+    axAmplitude.set_title("Cyclotron Radius")
+    axAmplitude.set_xlabel('t (s)')
+    axAmplitude.set_ylabel('Cyclotron Radius (m)')
+    figAmplitude.savefig("Images\Misc\CycloRadius")
 
     pyplot.close(fig3)
+    pyplot.close(figLarm)
+    pyplot.close(figAngFreq)
+    pyplot.close(figAmplitude)
 
 
 # Run the code
