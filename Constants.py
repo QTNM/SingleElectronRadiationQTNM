ca02e88e377bddfe41c2a0b383ab658ea1ba1443
DSwinnockPhysics <72457781+DSwinnockPhysics@users.noreply.github.com>
2025-03-12 12:31:30 +0000

    Add files via upload

--- Constants.py
+++ Constants.py
@@ -2,3 +2,4 @@ c = 299792458 # Speed of light in m/s
 eCharge = 1.602176634e-19 # Elementary charge in Coulombs
 eMass = 9.1093837015e-31 # Electron mass in kg
 k_B = 1.380649e-23 # Boltzmann constant in J/K
+vacPermittivity = 8.8541878128e-12 # F/m
